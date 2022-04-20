09167159c378971fceaa5358a8a1af2140d6826c
Andrew Widjaja <andrewdwidjaja@gmail.com>
2022-04-20 10:34:37 -0700

    Finished update functionality, added logo

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,17 @@ import 'antd/dist/antd.css';
 import './index.css';
 import NavContainer from './components/containers/NavContainer.jsx';
 import ItemsContainer from './components/containers/ItemsContainer.jsx';
+import LoginContainer from './components/containers/LoginContainer.jsx';
+import { useSelector, useDispatch } from 'react-redux';
 
 function App() {
+  const isLoggedIn = useSelector((state) => state.login.login);
+
   return (
     <div className="App">
-      <NavContainer></NavContainer>
-      <ItemsContainer></ItemsContainer>
+      <LoginContainer></LoginContainer>
+      {isLoggedIn === true && <NavContainer></NavContainer>}
+      {isLoggedIn === true && <ItemsContainer></ItemsContainer>}
     </div>
   );
 }
